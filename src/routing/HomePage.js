00bd116cd8c6fc59647d0424b3d9637d22519ed6
Rinkador <benoitmax16@gmail.com>
2024-02-09 15:51:22 +0100

    Merge prêt

--- src/routing/HomePage.js
+++ src/routing/HomePage.js
@@ -1,37 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Product from '../components/Product';
-// import ContactAdmin from '../components/Contact';
+// import Contact from '../components/Contact';
 
 const HomePage = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetch('https://api-3wa-ecomm-524fde41edfa.herokuapp.com/api/product')
-            .then(response => response.json())
-            .then(data => setProducts(data))
-            .catch(error => console.error("Erreur lors du chargement des produits depuis l'API:", error));
-    }, []);
-
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    
     return (
         <div className="homepage">
             <h1>Nos Produits</h1>
-                <div className="products-container">
-                    {products.map((product, index) => (
-                    <Product
-                    key={index}
-                    name={product.name}
-                    price={product.price}
-                    description={product.description}
-                />
-                ))}
-                </div>
-                {/*<div className="homepage">
-                    <Contact />
-                </div> */}
+            <div className="products-container">
+                <Product setFilteredProducts={setFilteredProducts} />
             </div>
-        );
-    };
+            <div className="products-container">
+                {filteredProducts.map(product => (
+                    <div key={product._id}>
+                        <h2>{product.name}</h2>
+                        <p>Prix: {product.price} €</p>
+                        <p>Description: {product.desc}</p>
+                        <p>Stock: {product.stock}</p>
+                        <p>Marque: {product.brand}</p>
+                        <p>Couleur: {product.color}</p>
+                    </div>
+                ))}
+            </div>
+            {/* <div className="contact">
+                <Contact />
+            </div> */}
+        </div>
+    );
+};
 
 export default HomePage;
-
-//Faker -> Fake données

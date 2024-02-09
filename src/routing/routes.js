10e5faf925329b8fb6a0ff0966f78b70b860cb53
Ludovic LOCHE <locheedwing@gmail.com>
2024-02-09 15:10:11 +0100

    login

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 import Layout from './Layout';
+import CartPage from './CartPage';
+import LoginPage from './LoginPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         children: [
-            { index: true, element: <HomePage />}
+            { index: true, element: <HomePage />},
+            { path: 'cart', element: <CartPage />},
+            { path: 'login', element: <LoginPage />},
+            { path: '/:email', element: <HomePage />},
         ]
     }
     
